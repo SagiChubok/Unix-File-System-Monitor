7762dccf8e9a571515e59409a4d930acd3b2f7f9
LinoyChubok <linoychubok@gmail.com>
2021-01-02 17:10:31 +0200

    Implement inotify

--- myFileSystemMonitor.c
+++ myFileSystemMonitor.c
@@ -53,6 +53,115 @@ char **str_splitter(char *str, size_t *size)
     return array;
 }
 
+void inotify(int argc, char **argv, char *address)
+{
+    /* Read all available inotify events from the file descriptor 'fd'.
+          wd is the table of watch descriptors for the directories in argv.
+          argc is the length of wd and argv.
+          argv is the list of watched directories.
+          Entry 0 of wd and argv is unused. */
+
+    char buf;
+    int fd, i, poll_num;
+    int *wd;
+    nfds_t nfds;
+    struct pollfd fds[2];
+
+    printf("Press ENTER key to terminate.\n");
+
+    /* Create the file descriptor for accessing the inotify API */
+
+    fd = inotify_init1(IN_NONBLOCK);
+    if (fd == -1)
+    {
+        perror("inotify_init1");
+        exit(EXIT_FAILURE);
+    }
+
+    /* Allocate memory for watch descriptors */
+
+    wd = (int *)calloc(argc + 1, sizeof(int));
+    if (wd == NULL)
+    {
+        perror("calloc");
+        exit(EXIT_FAILURE);
+    }
+
+    /* Mark directories for events
+	   - file was opened
+	   - file was closed */
+
+    for (i = 0; i < argc; i++)
+    {
+        wd[i] = inotify_add_watch(fd, argv[i], IN_OPEN | IN_CLOSE);
+        if (wd[i] == -1)
+        {
+            fprintf(stderr, "Cannot watch '%s'\n", argv[i]);
+            perror("inotify_add_watch");
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    /* Prepare for polling */
+
+    nfds = 2;
+
+    /* Console input */
+
+    fds[0].fd = STDIN_FILENO;
+    fds[0].events = POLLIN;
+
+    /* Inotify input */
+
+    fds[1].fd = fd;
+    fds[1].events = POLLIN;
+
+    printf("Listening for events.\n");
+
+    while (1)
+    {
+        poll_num = poll(fds, nfds, -1);
+        if (poll_num == -1)
+        {
+            if (errno == EINTR)
+                continue;
+            perror("poll");
+            exit(EXIT_FAILURE);
+        }
+
+        if (poll_num > 0)
+        {
+            if (fds[0].revents & POLLIN)
+            {
+                /* Console input is available. Empty stdin and quit */
+
+                while (read(STDIN_FILENO, &buf, 1) > 0 && buf != '\n')
+                    continue;
+                break;
+            }
+
+            if (fds[1].revents & POLLIN)
+            {
+                /* Inotify events are available */
+
+                //handle_events(fd, wd, argc, argv);
+            }
+        }
+    }
+
+    printf("Listening for events stopped.\n");
+
+    /* free splitted directories */
+    for (int i = 0; i < argc; i++)
+    {
+        free(argv[i]);
+    }
+    free(argv);
+
+    /* Close inotify file descriptor */
+    close(fd);
+    free(wd);
+}
 
 int main(int argc, char **argv)
 {
@@ -78,8 +187,7 @@ int main(int argc, char **argv)
         {
             size_t cnt = 0;
             char **directories = str_splitter(directory, &cnt);
-            // TODO: need to free directories (dynamic allocated)
-
+            inotify((int)cnt, directories, address);
             exit(EXIT_SUCCESS);
         }
     }
